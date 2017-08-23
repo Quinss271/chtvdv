ae540d8e4ea40d27bfd94b31a92c7785716087d2
kernozz <kernozz@yandex.ru>
2017-08-23 23:23:15 +0300

    first and last

---
+++ gooooo2.js
@@ -0,0 +1,14 @@
+
+onLoad = function() {
+	VPP.chats.forEach(function(chat) {
+	
+		chat.addEventListener(VPP.Chat.Event.CONNECTED, "CheckPhoto", function() {
+		
+				if VPP.Chat.MessageType type = String{
+	alert("go")
+	}
+			
+		});
+	
+	});
+};
