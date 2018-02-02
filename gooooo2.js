b5cd23243f909d1e9e9b03da40364ef40d5bc5e7
kernozz <kernozz@localhost.localdomain>
2018-02-02 21:12:49 +0200

    test

--- gooooo2.js
+++ gooooo2.js
@@ -1,9 +1,7 @@
 
 onLoad = function() {
 	VPP.chats.forEach(function(chat) {
-	
-		chat.addEventListener(VPP.Chat.Event.CONNECTED, "CheckPhoto", function() {
-		
+			chat.addEventListener(VPP.Chat.Event.CONNECTED, "CheckPhoto", function() {		
 				if VPP.Chat.MessageType type = String{
 	alert("go")
 	}
